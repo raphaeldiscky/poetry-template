2143195c1d7ecc6146af14f95b83d6cb1786b828
raphaeldiscky <raph.discky@gmail.com>
2025-07-15 17:27:33 +0700

    feat(deps): update dependencies, add husky and commitlint

---
+++ scripts/install-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+poetry install
+npm install
+
+# add husky hooks
+npx husky init
+echo "task format && task lint && git add -A ." > .husky/pre-commit
+echo "task test" > .husky/pre-push
+echo "npx --no-install commitlint --edit \$1" > .husky/commit-msg
--- src/example/example.py
+++ src/example/example.py
@@ -7,7 +7,7 @@ class Book:
 
 
 class Library:
-  def __init__(self) -> None:
+  def __init__(self):
     self.books: list[Book] = []
 
   def add_book(self, book: Book) -> None:
